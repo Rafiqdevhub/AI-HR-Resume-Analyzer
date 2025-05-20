df22cf21eeb17d7f5618d8f5867e0ac8598753ef
MUhammad Rafiq <rafiq85825@gmail.com>
2025-05-20 22:24:46 +0500

    Initialized the application with necessary installation.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold underline">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
